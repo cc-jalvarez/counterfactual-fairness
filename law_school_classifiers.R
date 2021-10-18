61203598e66f02c029a1704896852cc005ea9915
jalvarez <jose.alvarez@sns.it>
2021-10-18 16:54:44 +0200

    Updated data path

--- law_school_classifiers.R
+++ law_school_classifiers.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(caret)
 library(rstan)
 
-raw_data <- read.csv("law_data.csv")
+raw_data <- read.csv("data/law_data.csv")
 law <- dplyr::select(raw_data, race, sex, LSAT, UGPA, region_first, ZFYA, sander_index, first_pf) 
 law <- law[law$region_first != "PO",]
 law$region_first <- factor(law$region_first)
